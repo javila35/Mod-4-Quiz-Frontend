f5e916bcb16ec9625acd7a455aa6be884fa171c6
javila35 <josepharthuravila@gmail.com>
2020-05-31 20:33:23 -0700

    cleaning up dead API calls.

--- APItest.js
+++
@@ -1,8 +0,0 @@
-var url = 'http://newsapi.org/v2/top-headlines?' +
-          'sources=bbc-news&' +
-          'apiKey=c53d6160b14b48d68a124fce366c9f02';
-var req = new Request(url);
-fetch(req)
-    .then(function(response) {
-        console.log(response.json());
-    })
